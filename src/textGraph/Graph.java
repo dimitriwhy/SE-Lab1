fac677aa81a5979568c41656e018628ef3a321e3
dimitriwhy <en_why@live.cn>
2017-09-09 19:45:09 +0800

    init TextGraph.java, Graph.java

---
+++ src/textGraph/Graph.java
@@ -0,0 +1,5 @@
+package textGraph;
+
+public class Graph {
+
+}
---
+++ src/textGraph/TextGraph.java
@@ -0,0 +1,9 @@
+package textGraph;
+import textGraph.Graph;
+
+public class TextGraph {
+	
+	public static void main(String[] args) {
+		
+	}
+}
