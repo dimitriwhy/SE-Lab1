8a3b78acb7e2cf12f6f70080fcd6bea3e168e4e4
holynova-SD <holyalakir@outlook.com>
2017-09-09 21:53:57 +0800

    initialize the windwo.java

---
+++ src/textGraph/browseAction.java
@@ -0,0 +1,13 @@
+package textGraph;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+public class browseAction implements ActionListener{
+	public void actionPerformed(ActionEvent event) {
+		
+	}
+
+}
---
+++ src/textGraph/window.java
@@ -0,0 +1,68 @@
+package textGraph;
+
+import javax.swing.*;
+import java.awt.*;
+class window{
+	private JFrame mainFrame;
+	private JLabel label1;
+	public static JTextField fileLocation;
+	public static JButton browseFile;
+	public static JButton createGraph;
+	/**
+	 * Launch the application;
+	 * @param args
+	 */
+
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					window wd = new window();
+					wd.mainFrame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	/**
+	 * Create the application;
+	 */
+	public window() {
+		initialize();
+	}
+	/**
+	 * initialize the contents of the frame;
+	 */
+	private void initialize() {
+		
+		mainFrame = new JFrame("Textfile-Graph Convertor");
+		Container container = mainFrame.getContentPane();
+		mainFrame.setSize(400, 300);
+		mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		mainFrame.setLocationRelativeTo(null);
+		mainFrame.setResizable(false);
+		container.setLayout(null);
+		
+		label1 = new JLabel("Please enter or choose the path of the file:");
+		label1.setBounds(30, 10, 300, 20);
+		container.add(label1);
+		
+		fileLocation = new JTextField();
+		fileLocation.setBounds(50, 50, 200, 30);
+		container.add(fileLocation);
+		
+		browseFile = new JButton("Select...");
+		browseFile.setBounds(270, 50, 100, 30);
+		browseFile.addActionListener(new browseAction());
+		container.add(browseFile);
+		
+		createGraph = new JButton("Create Graph");
+		createGraph.setBounds(150, 150, 100, 50);
+		createGraph.addActionListener(new allFunction());
+		container.add(createGraph);
+		
+	}
+	
+}
