995f2003643a1bd5c687015a67e443ce7b822488
dimitriwhy <en_why@live.cn>
2017-09-09 21:19:04 +0800

    feat: output bridge words (queryBridgeWords).

--- src/textGraph/TextGraph.java
+++ src/textGraph/TextGraph.java
@@ -1,8 +1,25 @@
 package textGraph;
 import textGraph.Graph;
-
+import java.util.*;
 public class TextGraph {
-	
+	Graph createDirectedGraph(String filename) {
+		
+	}
+	String queryBridgeWords(Graph G, String word1, String word2) {
+		List<Integer> bridgeList = G.getBridges(word1, word2);
+		String output;
+		if (bridgeList.size() == 0) {
+			output = "No bridge words from "+word1+" to "+word2+"!";
+		}else {
+			output = "The bridge words from "+word1+" to "+word2+" are: ";
+			for (int i = 0; i < bridgeList.size(); i++) {
+				if  (i != bridgeList.size() - 1)
+					output += " " + G.getName(bridgeList.get(i))+",";
+				else output += " and "+G.getName(bridgeList.get(i))+".";
+			}
+		}
+		return output;
+	}
 	public static void main(String[] args) {
 		
 	}
